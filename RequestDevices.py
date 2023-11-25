86defe2b1b7bf8314065dc856de772e788658cca
zzqmatebook <475411577@qq.com>
2023-11-25 15:55:10 +0800

    完成了接口的基本抽象工作，只需要传入参数就可以获取设备的任意读数了，剩下的就是可配置化以及写入日志的工作了

--- RequestDevices.py
+++ RequestDevices.py
@@ -1,46 +1,96 @@
 import requests as req
-import json
 
 class RequestsDeviceObj:
-    def requeststo(self,uri,dev_type,hd_name):
-        host="10.11.17.244"
-        url=F"https://{host}{uri}"
-        print(f"传入的参数是:{url}")
-        headers = {
-            "Content-Type": "application/json",
-        }
+    def __init__(self,hostip):
+        self.host=hostip
+    def __requestObj(self,req_data,uri):
+        try:
+            url=F"https://{self.host}{uri}"
+            print(f"传入的参数是:{url}")
+            headers = {
+                "Content-Type": "application/json",
+            }
+            data = req_data
+            response = req.post(url, json=data, headers=headers,verify=False)
+            return response
+        except Exception as e:
+            print(e)
+            return e
+    def requestsAccessToken(self,secret,uri):
+        # url=F"https://{self.host}{self.uri}"
+        # print(f"传入的参数是:{url}")
+        # headers = {
+        #     "Content-Type": "application/json",
+        # }
+
         data = {
             # "accessToken": "30612ab31213e7c1ad41ca5603fe883a",
-            "accessToken": "a733bc4719f04d510a4d161b80bb00d6",
+            "secret": secret
+        }
+        response = self.__requestObj(req_data=data,uri=uri)
+        if response.status_code == 200:
+            result_dict = response.json()
+            result=result_dict.get("data",[{}])[0].get("accessToken")
+            print(result)
+            return result
+        else:
+            print(f"Error: {response.status_code}, {response.text}")
+
+    def requestDeviceData(self,acc_token,hd_typeid,uri):
+        data = {
+            # "accessToken": "30612ab31213e7c1ad41ca5603fe883a",
+            "accessToken": acc_token,
             "filter": {
                 "all": True,
                 "hardwareClassifications": ["terminal"],
-                "hardwareTypeIDs": ["e1c8167f277f8266954b72b47fb44797"]
+                "hardwareTypeIDs": [hd_typeid]
             }
         }
-        response = req.post(url, json=data, headers=headers,verify=False)
+        response = self.__requestObj(req_data=data,uri=uri)
         if response.status_code == 200:
-            result_list = response.json()
-            print(result_list)
-            # result_dict=json.loads(result)
-            hard_list=result_list.get('data', {}).get('results', [])
-            debug_mode = True
-            for hard in hard_list:
-                hardwareName=hard.get('info',[]).get("hardwareName")
-                if hardwareName == hd_name:
-                    print(f"温度区域：{hd_name}")
-                    states_list=hard.get('states', [])
-                    print(states_list)
-                    for state in states_list:
-                        if state.get('stateID')==dev_type:
-                            reported_value = state.get('reported')
-                            if debug_mode:
-                                if dev_type=="DEV_MOISTURE":
-                                    print(f"设备湿度: {reported_value}")
-                                elif dev_type=="DEV_TEMPRATURE":
-                                    print(f"设备温度: {reported_value}")
-                            else:
-                                print(reported_value)
+            return response
+        else:
+            print(f"Error: {response.status_code}, {response.text}")
+    def analyticalRequestsdata(self,response,dev_type,hd_name):
+        # host="10.11.17.244"
+        # url=F"https://{self.host}{self.uri}"
+        # print(f"传入的参数是:{url}")
+        # headers = {
+        #     "Content-Type": "application/json",
+        # }
+        # data = {
+        #     # "accessToken": "30612ab31213e7c1ad41ca5603fe883a",
+        #     "accessToken": acc_token,
+        #     "filter": {
+        #         "all": True,
+        #         "hardwareClassifications": ["terminal"],
+        #         "hardwareTypeIDs": [hd_typeid]
+        #     }
+        # }
+        # response = req.post(url, json=data, headers=headers,verify=False)
+        # response = self.__requestObj(req_data=data)
+        # if response.status_code == 200:
+        result_list = response.json()
+        print(result_list)
+        # result_dict=json.loads(result)
+        hard_list=result_list.get('data', {}).get('results', [])
+        for hard in hard_list:
+            hardwareName=hard.get('info',[]).get("hardwareName")
+            if hardwareName == hd_name:
+                print(f"温度区域：{hd_name}")
+                states_list=hard.get('states', [])
+                print(states_list)
+                for state in states_list:
+                    if state.get('stateID')==dev_type:
+                        reported_value = state.get('reported')
+                        print(reported_value)
+                        if dev_type=="DEV_MOISTURE":
+                            print(f"设备湿度: {reported_value}")
+                        elif dev_type=="DEV_TEMPRATURE":
+                            print(f"设备温度: {reported_value}")
+
+
+                        #
                         # if dev_type=="DEV_TEMPRATURE":
                         #     reported_value = state.get('reported')
                         #     print(f"设备温度: {reported_value}")
@@ -56,8 +106,11 @@ class RequestsDeviceObj:
                     #         print(f"设备温度: {reported_value}")
                     # else:
                     #     print(f"Error:{state}")
-        else:
-            print(f"Error: {response.status_code}, {response.text}")
+        # else:
+        #     print(f"Error: {response.status_code}, {response.text}")
+        #     result_dict=response.json()
+
+
         #     states_list = result_list.get('data', {}).get('results', [])[0].get('states', [])
         #     for state in states_list:
         #         print(state)
--- main.py
+++ main.py
@@ -1,17 +1,53 @@
 from RequestDevices import RequestsDeviceObj as rdo
-
-
+from DeviceInfo import Device as dev
+import time
 def requestActive():
     # Use a breakpoint in the code line below to debug your script.
-    requestTo=rdo()
-    uri="/iotp/api/open/deviceManagement/hardware/list"
+    hd_typeid=dev.ROOM_TEMPERATURE_HUMIDITY_2.value
+
+
+    uri="/iotp/api/open/accessControl/accessToken/get"
+    uri_devdata="/iotp/api/open/deviceManagement/hardware/list"
+    host="10.11.17.244"
+    secretKey="2fe886a731177cedd3d8a7c400f3d773"
     dev_type="DEV_MOISTURE"
     hd_name="配电柜温湿度1"
-    requestTo.requeststo(uri,dev_type,hd_name)
+    requestTo = rdo(hostip=host)
+    acc_token=requestTo.requestsAccessToken(secret=secretKey,uri=uri)
+    # response=requestTo.requestDeviceData(acc_token=acc_token,hd_typeid=hd_typeid,uri=uri_devdata)
+    # requestTo.analyticalRequestsdata(response=response,dev_type=dev_type,hd_name=hd_name)
 
+    #空调的参数
+    hd_typeid_kt=dev.PRECISION_AIR_CONDITIONER_MIDEA.value
+    dev_type_kt="UNIT_TEMP_OUTDOOR"
+    hd_name_kt="精密空调-美的-MAV013WT1N2S-电总QJF1350298-8-5-1"
+    # resp_kt=requestTo.requestDeviceData(acc_token,hd_typeid_kt,uri_devdata)
+    # requestTo.analyticalRequestsdata(response=resp_kt,dev_type=dev_type_kt,hd_name=hd_name_kt)
+    # requestTo.requeststo(uri,dev_type,hd_name,hd_typeid)
+
+    #电量仪
+    hd_typeid_dly=dev.POWER_METER.value
+    dev_type_dly="INPUT_VOL_3"
+    hd_name_dly="电量仪"
+    resp_dly=requestTo.requestDeviceData(acc_token=acc_token,hd_typeid=hd_typeid_dly,uri=uri_devdata)
+    requestTo.analyticalRequestsdata(response=resp_dly,dev_type=dev_type_dly,hd_name=hd_name_dly)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    now_starttime = time.strftime("%Y-%m-%dT%H:%M:%S", time.localtime())
+    msg = f"\n==========程序开始时间：{now_starttime}==================\n"
+    print(msg)
+
+    start_runtim = time.time()
+    # runDelete()    #调用的方法
     requestActive()
+    end_runtime = time.time()
+    calculation_time = end_runtime - start_runtim
+    msg1 = f"程序完成运行花费时间：{calculation_time:.6f}秒"
+    print(msg1)
+    now_endtime = time.strftime("%Y-%m-%dT%H:%M:%S", time.localtime())
+    msg = f"\n==============程序结束时间：{now_endtime}==============\n"
+    print(msg)
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
